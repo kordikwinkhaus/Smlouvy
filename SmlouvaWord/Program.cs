83b6849ef44320847ea946b6ec87017191cb3fd6
Tomas Kordik <kordik@winkhaus.cz>
2016-11-01 13:48:57 +0100

    Oznamování chyb

--- SmlouvaWord/Program.cs
+++ SmlouvaWord/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Windows.Forms;
 using WinkhausCR.Bugs;
 
 namespace SmlouvaWord
 {
     static class Program
     {
+        private const string TITLE = "Export smlouvy";
+
         [STAThread]
         static void Main(string[] args)
         {
@@ -19,12 +22,13 @@ namespace SmlouvaWord
                 if (wrapper.MissingFields.Count != 0)
                 {
                     string fieldNames = string.Join(", ", wrapper.MissingFields);
-                    string msg = "Pro některá pole v dokumentu nebyla nalezena hodnota. Názvy polí: " + fieldNames;
-
+                    string msg = "Pro některá pole v dokumentu nebyla nalezena hodnota.\r\nNázvy polí: " + fieldNames + ".";
+                    MessageBox.Show(msg, TITLE, MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
             catch (Exception ex)
             {
+                MessageBox.Show("Nastala chyba během vytváření smlouvy. Zkontrolujte prosím chybový log.", TITLE, MessageBoxButtons.OK, MessageBoxIcon.Error);
                 Logger.Instance.Log(ex);
             }
         }
